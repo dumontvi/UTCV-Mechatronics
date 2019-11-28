4d29deb11b680495948b9b9470071acd2a45d2d2
reinali07 <reinali07@gmail.com>
2019-11-27 19:37:53 -0500

    motor+color sensor after 1st integration

--- src/motor_sensor/motor_colour_sensor.ino
+++ src/motor_sensor/motor_colour_sensor.ino
@@ -1,13 +1,11 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_60X);
 
 
 // Definitions Arduino pins connected to input H Bridge
-int ENA = 8;
-int IN1 = 7;
-int IN2 = 6;
-
+@ -11,10 +11,10 @@ int IN2 = 6;
 void setup()
 {
 // Set the output pins
@@ -15,18 +13,23 @@ pinMode(IN1, OUTPUT);
 pinMode(IN2, OUTPUT);
 pinMode(ENA, OUTPUT);
 
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+//Check if sensor found
   Serial.begin(9600);
   if (tcs.begin()) {
     Serial.println("Found sensor");
-  } else {
-    Serial.println("No TCS34725 found ... check your connections");
-  while (1);
-  }
+@ -25,29 +25,84 @@ pinMode(ENA, OUTPUT);
 
 }
 
 void loop()
 {
+char rx_byte = 0; //holds input character
+int start_time;
+int end_time;
+int duration;
 
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
@@ -36,18 +39,86 @@ void loop()
   delay(50);
   tcs.getRGB(&red,&green,&blue);
   tcs.setInterrupt(true);  // turn off LED
+int initr=0; //initial vals
+int initg=0;
+int initb=0;
+int prevr=0; //previous vals
+int prevg=0;
+int prevb=0;
+int red=0; //current vals
+int green=0;
+int blue=0;
+int dprevr=0; //difference from previous vals
+int dprevg=0;
+int dprevb=0;
+
+void loop()
+{
+  prevr=red; //current vals from prev cycle become prev vals
+  prevg=green;
+  prevb=blue;
+  
+  //color sensor
+  delay(50); //checks things every 50 ms(?) can increase delay to increase time between every update
+  tcs.getRGB(&red,&green,&blue); //update current vals
 
   Serial.print("R:\t"); Serial.print(int(red)); 
   Serial.print("\tG:\t"); Serial.print(int(green)); 
   Serial.print("\tB:\t"); Serial.println(int(blue));
+
+  //check input
+  if (Serial.available() > 0) {
+    rx_byte = Serial.read();
+    if (rx_byte == 'a'){ //if a, turn on motor
+      start_time = millis(); //start timer
+      Serial.print("start\t");
+      Serial.println(start_time);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      analogWrite(ENA,255);
+      initr = red; //set initial colour
+      initg = green;
+      initb = blue;
+    }
+  }
   
   if ((red>=200||red>=240)&&(green>=18||green<=35)&&(blue>=23||blue<=38)){
     Serial.println("STOP THE MOTOR");
     analogWrite(ENA,0);
     delay(100);
+  dprevr = abs(red-prevr); //difference w/ prev vals
+  dprevg = abs(green-prevg);
+  dprevb = abs(blue-prevb);
+
+  if (abs(red-initr)!=0||abs(green-initg)!=0||abs(blue-initb)!=0){ //if at least one of current vals differ from initial
+    Serial.println(dprevr); 
+    Serial.println(dprevg);  
+    Serial.println(dprevb);
   }
   else {
     analogWrite(ENA,200);
     delay(50);
+
+  if (dprevr==0&&dprevg==0&&dprevb==0){ //if no change. maybe make this a range or increase time between updates
+    Serial.println("STOP");
+    analogWrite(ENA,0); //stop motor
+    end_time = millis(); //stop timer
+    duration = (end_time - start_time)/1000; //time elapsed
+    Serial.print("end:\t");Serial.print(end_time);
+    Serial.print("\tstart:\t");Serial.print(start_time);
+    Serial.print("\tduration:\t");Serial.print(duration);
+    while(1); //infinite loop
   }
+}
+  
+  /*if (red>=93&&green<=92&&blue<=75){
+    Serial.println("STOP THE MOTOR");
+    analogWrite(ENA,0);
+    end_time = millis();
+    duration = (end_time - start_time)/1000;
+    Serial.print("end:\t");Serial.print(end_time);
+    Serial.print("\tstart:\t");Serial.print(start_time);
+    Serial.print("\tduration:\t");Serial.print(duration);
+    while(1);
+  }*/
 }
