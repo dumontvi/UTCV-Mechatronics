b76fcd0f63ebf78090eb4ac16b33af27b7631e88
reinali07 <reinali07@gmail.com>
2019-11-29 23:06:31 -0500

    stops motor when spike in colour change

--- src/Embedded/motor_sensor/motor_colour_sensor.ino
+++ src/Embedded/motor_sensor/motor_colour_sensor.ino
@@ -1,18 +1,16 @@
+#include <Adafruit_TCS34725.h>
 #include <Wire.h>
-#include "Adafruit_TCS34725.h"
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_60X);
 
 
 // Definitions Arduino pins connected to input H Bridge
-@ -11,10 +11,10 @@ int IN2 = 6;
+int ENA = 8;
+int IN1 = 7;
+int IN2 = 6;
+
 void setup()
 {
 // Set the output pins
-pinMode(IN1, OUTPUT);
-pinMode(IN2, OUTPUT);
-pinMode(ENA, OUTPUT);
-
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(ENA, OUTPUT);
@@ -20,34 +18,28 @@ pinMode(ENA, OUTPUT);
   Serial.begin(9600);
   if (tcs.begin()) {
     Serial.println("Found sensor");
-@ -25,29 +25,84 @@ pinMode(ENA, OUTPUT);
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+  while (1);
+  }
 
 }
 
-void loop()
-{
 char rx_byte = 0; //holds input character
 int start_time;
 int end_time;
 int duration;
 
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
 
-  float red, green, blue;
-  tcs.setInterrupt(false);  // turn on LED
-  delay(50);
-  tcs.getRGB(&red,&green,&blue);
-  tcs.setInterrupt(true);  // turn off LED
 int initr=0; //initial vals
 int initg=0;
 int initb=0;
 int prevr=0; //previous vals
 int prevg=0;
 int prevb=0;
-int red=0; //current vals
-int green=0;
-int blue=0;
+float red=0; //current vals
+float green=0;
+float blue=0;
 int dprevr=0; //difference from previous vals
 int dprevg=0;
 int dprevb=0;
@@ -59,18 +51,18 @@ void loop()
   prevb=blue;
   
   //color sensor
-  delay(50); //checks things every 50 ms(?) can increase delay to increase time between every update
+  delay(10); //poll every 10 ms
   tcs.getRGB(&red,&green,&blue); //update current vals
 
-  Serial.print("R:\t"); Serial.print(int(red)); 
-  Serial.print("\tG:\t"); Serial.print(int(green)); 
-  Serial.print("\tB:\t"); Serial.println(int(blue));
+  //Serial.print("R:\t"); Serial.print(int(red)); 
+  //Serial.print("\tG:\t"); Serial.print(int(green)); 
+  //Serial.print("\tB:\t"); Serial.println(int(blue));
 
   //check input
   if (Serial.available() > 0) {
     rx_byte = Serial.read();
     if (rx_byte == 'a'){ //if a, turn on motor
-      start_time = millis(); //start timer
+      start_time = millis()/1000; //start timer
       Serial.print("start\t");
       Serial.println(start_time);
       digitalWrite(IN1, HIGH);
@@ -82,36 +74,31 @@ void loop()
     }
   }
   
-  if ((red>=200||red>=240)&&(green>=18||green<=35)&&(blue>=23||blue<=38)){
-    Serial.println("STOP THE MOTOR");
-    analogWrite(ENA,0);
-    delay(100);
   dprevr = abs(red-prevr); //difference w/ prev vals
   dprevg = abs(green-prevg);
   dprevb = abs(blue-prevb);
 
-  if (abs(red-initr)!=0||abs(green-initg)!=0||abs(blue-initb)!=0){ //if at least one of current vals differ from initial
-    Serial.println(dprevr); 
-    Serial.println(dprevg);  
-    Serial.println(dprevb);
-  }
-  else {
-    analogWrite(ENA,200);
-    delay(50);
-
-  if (dprevr==0&&dprevg==0&&dprevb==0){ //if no change. maybe make this a range or increase time between updates
+  //if (abs(red-initr)!=0||abs(green-initg)!=0||abs(blue-initb)!=0){ //if at least one of current vals differ from initial
+  Serial.print(dprevr); //print differences
+  Serial.print(", "); 
+  Serial.print(dprevg); 
+  Serial.print(", "); 
+  Serial.print(dprevb);
+  Serial.print("\n");
+  //}
+  
+  if (dprevr>10&&dprevr<200&&dprevg>10&&dprevg<200&&dprevb>10&&dprevb<200){ //when spike occurs (ignore initial spike when first turned on)
     Serial.println("STOP");
     analogWrite(ENA,0); //stop motor
-    end_time = millis(); //stop timer
-    duration = (end_time - start_time)/1000; //time elapsed
+    end_time = millis()/1000; //stop timer
+    duration = (end_time - start_time); //time elapsed
     Serial.print("end:\t");Serial.print(end_time);
     Serial.print("\tstart:\t");Serial.print(start_time);
     Serial.print("\tduration:\t");Serial.print(duration);
-    while(1); //infinite loop
+    while(1); //infinite loop, can implement in such a way that if 'a' is entered again, will restart. need to put all the code in a function and call it in a condition inside the while loop.
   }
-}
-  
-  /*if (red>=93&&green<=92&&blue<=75){
+  /*
+  if (red>=93&&green<=92&&blue<=75){
     Serial.println("STOP THE MOTOR");
     analogWrite(ENA,0);
     end_time = millis();
